54798771899ef5eaeb2fa8d687cd1d22f6cb47df
Your GitHub Username <your-email@example.com>
2026-09-05 06:49:53 +0530

    Fix package.json

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,13 +3,7 @@ import "./../styles/navbar.css";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">🌸 Happy Teacher's Day 🌸</div>
-
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Gallery</a></li>
-        <li><a href="#">Message</a></li>
-      </ul>
+      <h1>🌸 Happy Teacher's Day 🌸</h1>
     </nav>
   );
 }
